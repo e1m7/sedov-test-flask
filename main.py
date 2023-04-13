48244c20d4753b6f70b05cb217b5233e9823b73e
e1m7 <91698337+e1m7@users.noreply.github.com>
2023-04-13 18:30:40 +0000

    Add Validation form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 SECRET_KEY = os.urandom(32)
@@ -15,9 +15,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email_address = db.Column(db.String(length=50),
-                              nullable=False,
-                              unique=True)
+    email_address = db.Column(db.String(length=50),nullable=False,unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
@@ -28,9 +26,7 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024),
-                            nullable=False,
-                            unique=True)
+    description = db.Column(db.String(length=1024),nullable=False,unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
@@ -41,13 +37,26 @@ class Item(db.Model):
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User name:')
-    email_address = StringField(label='Email address:')
-    password1 = PasswordField(label='Password:')
-    password2 = PasswordField(label='Password repeat:')
+    username = StringField(
+        label='User Name:', 
+        validators=[Length(min=2, max=30), DataRequired()]
+    )
+    email_address = StringField(
+        label='Email Address:', 
+        validators=[Email(), DataRequired()]
+    )
+    password1 = PasswordField(
+        label='Password:', 
+        validators=[Length(min=6), DataRequired()]
+    )
+    password2 = PasswordField(
+        label='Confirm Password:', 
+        validators=[EqualTo('password1'), DataRequired()]
+    )
     submit = SubmitField(label='Create Account')
 
 
@@ -66,9 +75,21 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+            username=form.username.data,
+            email_address=form.email_address.data,
+            password_hash=form.password1.data
+        )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(err_msg)
     return render_template('register.html', form=form)
 
 

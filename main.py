680f14e10cf5628caa19f35d8804183789503046
e1m7 <91698337+e1m7@users.noreply.github.com>
2023-04-15 18:37:42 +0000

    Add Flash messages and unique names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 SECRET_KEY = os.urandom(32)
@@ -15,7 +15,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email_address = db.Column(db.String(length=50),nullable=False,unique=True)
+    email_address = db.Column(db.String(length=50),
+                              nullable=False,
+                              unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
@@ -26,7 +28,9 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024),nullable=False,unique=True)
+    description = db.Column(db.String(length=1024),
+                            nullable=False,
+                            unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
@@ -37,26 +41,31 @@ class Item(db.Model):
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(
-        label='User Name:', 
-        validators=[Length(min=2, max=30), DataRequired()]
-    )
-    email_address = StringField(
-        label='Email Address:', 
-        validators=[Email(), DataRequired()]
-    )
-    password1 = PasswordField(
-        label='Password:', 
-        validators=[Length(min=6), DataRequired()]
-    )
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+            
+    username = StringField(label='User Name:',
+                           validators=[Length(min=2, max=30),
+                                       DataRequired()])
+    email_address = StringField(label='Email Address:',
+                                validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:',
+                              validators=[Length(min=6),
+                                          DataRequired()])
     password2 = PasswordField(
-        label='Confirm Password:', 
-        validators=[EqualTo('password1'), DataRequired()]
-    )
+        label='Confirm Password:',
+        validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 
@@ -79,17 +88,16 @@ def market_page():
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(
-            username=form.username.data,
-            email_address=form.email_address.data,
-            password_hash=form.password1.data
-        )
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
-    if form.errors != {}:
+    if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            print(err_msg)
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
     return render_template('register.html', form=form)
 
 

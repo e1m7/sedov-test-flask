111fc34a3b9c77beac083c0b75617444cc64a656
e1m7 <91698337+e1m7@users.noreply.github.com>
2023-04-18 08:06:18 +0000

    Add Login user in App

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+
 import os
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_login import login_user
+from flask_login import UserMixin
 
 SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = SECRET_KEY
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 # --------------------------- CLASSES -------------------------------------------
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email_address = db.Column(db.String(length=50),
-                              nullable=False,
-                              unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
-    items = db.relationship('Item', backref='owned_user', lazy=True)
+    items = db.relationship("Item", backref="owned_user", lazy=True)
 
     @property
     def password(self):
@@ -30,7 +38,12 @@ class User(db.Model):
 
     @password.setter
     def password(self, plain_text_password):
-        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode(
+            "utf-8"
+        )
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 
 class Item(db.Model):
@@ -38,13 +51,11 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024),
-                            nullable=False,
-                            unique=True)
-    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey("user.id"))
 
     def __repr__(self):
-        return f'Item {self.name}'
+        return f"Item {self.name}"
 
 
 # --------------------------- FORMS -------------------------------------------
@@ -58,67 +69,98 @@ class RegisterForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
         if user:
-            raise ValidationError('Username already exists! Please try a different username')
+            raise ValidationError(
+                "Username already exists! Please try a different username"
+            )
 
     def validate_email_address(self, email_address_to_check):
-        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        email_address = User.query.filter_by(
+            email_address=email_address_to_check.data
+        ).first()
         if email_address:
-            raise ValidationError('Email Address already exists! Please try a different email address')
-            
-    username = StringField(label='User Name:',
-                           validators=[Length(min=2, max=30),
-                                       DataRequired()])
-    email_address = StringField(label='Email Address:',
-                                validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:',
-                              validators=[Length(min=6),
-                                          DataRequired()])
+            raise ValidationError(
+                "Email Address already exists! Please try a different email address"
+            )
+
+    username = StringField(
+        label="User Name:", validators=[Length(min=2, max=30), DataRequired()]
+    )
+    email_address = StringField(
+        label="Email Address:", validators=[Email(), DataRequired()]
+    )
+    password1 = PasswordField(
+        label="Password:", validators=[Length(min=6), DataRequired()]
+    )
     password2 = PasswordField(
-        label='Confirm Password:',
-        validators=[EqualTo('password1'), DataRequired()])
-    submit = SubmitField(label='Create Account')
+        label="Confirm Password:", validators=[EqualTo("password1"), DataRequired()]
+    )
+    submit = SubmitField(label="Create Account")
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label='User Name:', validators=[DataRequired()])
-    password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Sign In')
+    username = StringField(label="User Name:", validators=[DataRequired()])
+    password = PasswordField(label="Password:", validators=[DataRequired()])
+    submit = SubmitField(label="Sign In")
+
 
 # --------------------------- ROUTERS -------------------------------------------
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home_page():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/market')
+@app.route("/market")
 def market_page():
     items = Item.query.all()
-    return render_template('market.html', items=items)
+    return render_template("market.html", items=items)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,
-                              email_address=form.email_address.data,
-                              password=form.password1.data)
+        user_to_create = User(
+            username=form.username.data,
+            email_address=form.email_address.data,
+            password=form.password1.data,
+        )
         db.session.add(user_to_create)
         db.session.commit()
-        return redirect(url_for('market_page'))
-    if form.errors != {}: #If there are not errors from the validations
+        return redirect(url_for("market_page"))
+    if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+            flash(
+                f"There was an error with creating a user: {err_msg}", category="danger"
+            )
 
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login_page():
     form = LoginForm()
-    return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(
+                f"Success! You are loggin in as: {attempted_user.username}",
+                category="success",
+            )
+            return redirect(url_for("market_page"))
+        else:
+            flash(
+                f"Username or password are not match! Please try again!",
+                category="danger",
+            )
+
+    return render_template("login.html", form=form)
 
 
-app.run(host='0.0.0.0', port=81, use_reloader=True)
+app.run(host="0.0.0.0", port=81, use_reloader=True)
+
